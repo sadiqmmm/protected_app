13ef29ab84e22f54e8ef2afbbff7533e76978530
Jorge Coca <jcoca@redpointtech.com>
2013-02-04 15:54:28 -0600

    Changed view for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,7 @@ class ItemsController < ApplicationController
   # GET /items/1
   # GET /items/1.json
   def show
-    @item = Item.find(params[:id])
+    @item = current_user.items.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
