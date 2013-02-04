2a337bfff13a0827721e628c67dfdc37969e1621
Jorge Coca <jcoca@redpointtech.com>
2013-02-04 16:06:20 -0600

    Added typeahead to search bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :get_items
+
+  def get_items
+    @search_items = current_user.items.order("title").pluck(:title)
+  end
 end
